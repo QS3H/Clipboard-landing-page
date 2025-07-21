c2cc21f27f309047305645699a0f29c18db459d2
shawky <mastershawky2017@gmail.com>
2025-07-21 22:11:42 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        green: "hsl(171, 66%, 44%)",
+        blue: "hsl(233, 100%, 69%)",
+        darkGray: "hsl(210, 10%, 33%)",
+        gray: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
